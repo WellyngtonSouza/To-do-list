49f7e72e443010e63c648c0cf0b11f9820422602
wellyngton souza <wellyngtonmonteiro84@gmail.com>
2023-01-13 20:32:59 -0300

    aprendendo localstorage

--- assets/js.js
+++ assets/js.js
@@ -2,36 +2,69 @@ let input = document.querySelector("#input");
 let botao = document.querySelector("#button");
 
 botao.addEventListener("click", resultado);
-input.addEventListener("keydown", tecla)
+input.addEventListener("keydown", tecla);
 
-function tecla(tecla) {
-    if (tecla.which == 13){
-        resultado()
-    }
-    return tecla
+function limpar() {
+  localStorage.clear();
+  alert("Reinicie a página (F5)")
 }
 
+let caixa = [];
+let caixaSalva = [];
+let conteudoSalvo;
+
+function tecla(tecla) {
+  if (tecla.which == 13) {
+    resultado();
+  }
+  return tecla;
+}
+
+function criarElemento(conteudoFinal) {
+  let paiUl = document.querySelector("#list"); /// ul
+
+  let NovaLi = document.createElement("li"); // criando o elemento li onde vai ficar o checkbox e o conteúdo ao lado
+
+  let content = document.createElement("p");
+  let newCheckbox = document.createElement("input"); //criando o checkbox
+
+  newCheckbox.setAttribute("type", "checkbox");
+  newCheckbox.setAttribute("id", "check"); // atributos
+
+  paiUl.appendChild(NovaLi);
+  NovaLi.appendChild(newCheckbox);
+
+  content.innerHTML = conteudoFinal; //criando o conteúdo
+  NovaLi.appendChild(content);
+}
+let paiUl = document.querySelector("#list"); /// ul
 function resultado() {
   if (input.value == input.value.length) {
     alert("por favor coloque um dado válido");
   } else {
-    let paiUl = document.querySelector("#list"); /// ul
 
-    let NovaLi = document.createElement("li"); // criando o elemento li onde vai ficar o checkbox e o conteúdo ao lado
+    criarElemento(input.value);
+    input.value = ""; // reinicia o valor após a escolha
 
-    let content = document.createElement("span");
-    let newCheckbox = document.createElement("input"); //criando o checkbox
+    for (let i = 0; i < document.getElementsByTagName("li").length; i++) {
+      let conteudo = paiUl.children[i].children[1].innerText;
+      localStorage.setItem("conteudo", conteudo);
 
-    newCheckbox.setAttribute("type", "checkbox");
-    newCheckbox.setAttribute("id", "check"); // atributos
+      caixa[i] = localStorage.getItem("conteudo");
 
-    paiUl.appendChild(NovaLi);
-    NovaLi.appendChild(newCheckbox);
-
-    content.innerHTML = input.value; //criando o conteúdo
-    NovaLi.appendChild(content);
-
-    input.value = ""; // reinicia o valor apoś a escolha
+      localStorage.setItem("caixa", caixa);
+    }
   }
 }
-//
+
+caixaSalva = localStorage.getItem("caixa").split(",");
+
+if (localStorage.getItem("conteudo") !== null) {
+  console.log("deu certo");
+  let inputLimpar = document.getElementById("limpar")
+  inputLimpar.style.display = "inline"
+  
+  for (let i = 0; i < caixaSalva.length; i++) {
+    criarElemento(caixaSalva[i])
+  }
+}
