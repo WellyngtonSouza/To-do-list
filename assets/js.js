7184f541e761ee44ef662fec3e3f54b131582c20
wellyngton souza <wellyngtonmonteiro84@gmail.com>
2023-02-15 17:19:52 -0300

    update all

--- assets/js.js
+++ assets/js.js
@@ -1,106 +1,64 @@
-// elementos HTML
-
-let input = document.querySelector("#input");
-let botao = document.querySelector("#button");
-let paiUl = document.querySelector("#list");
-
-// contador
-
-let cont;
-
-if(localStorage.getItem("numero") == null){
-  cont = 0;
-  
-
-}else{
-  cont = Number(JSON.parse(localStorage.getItem("numero")));
-  
-}
-
-//arrays
-
-let conteudoSalvo;
-let real;
-if(cont == 0){
-   conteudoSalvo = [];
-   real = []
-}else{
-   conteudoSalvo = JSON.parse(localStorage.getItem("stringone"))
-   real = JSON.parse(localStorage.getItem("storage"))
-}
-
-
-// eventos
-botao.addEventListener("click", resultado);
-input.addEventListener("keydown", tecla);
+const buttonAdd = document.getElementById("button")
+const buttonClear = document.getElementById("limpar")
+const input = document.getElementById("input")
 
 
 
-//funções
-function tecla(tecla) {
-  if (tecla.which == 13) {
-    resultado();
-  }
-  return tecla;
-}
 
-function limpar() {
-  localStorage.clear();
-  alert("Reinicie a página (F5)");
-}
 
-function criarElemento(conteudoFinal) {
-  let paiUl = document.querySelector("#list"); /// ul
-
-  let NovaLi = document.createElement("li"); // criando o elemento li onde vai ficar o checkbox e o conteúdo ao lado
-
-  let content = document.createElement("p");
-  let newCheckbox = document.createElement("input"); //criando o checkbox
-
-  newCheckbox.setAttribute("type", "checkbox");
-  newCheckbox.setAttribute("id", "check"); // atributos
-
-  paiUl.appendChild(NovaLi);
-  NovaLi.appendChild(newCheckbox);
-
-  content.innerHTML = conteudoFinal; //criando o conteúdo
-  NovaLi.appendChild(content);
-}
-
-function lstorage() {
-  return {
-    content: paiUl.children[i].children[1].innerText,
-  };
-}
-
-// contador
-
-function resultado() {
-  if (input.value == input.value.length) {
-    alert("por favor coloque um dado válido");
+let addElement = function () {
+  if (input.value == "") {
+    alert("não tem nenhum conteúdo no campo!!")
   }
   else {
-    criarElemento(input.value);
-    input.value = "";
-    for (i = cont; i < document.getElementsByTagName("li").length; i++) {
-      conteudoSalvo.push({
-        content: paiUl.children[i].children[1].innerText,
-      });
-      
-    }
-    real.push(conteudoSalvo[conteudoSalvo.length - 1]);
-    cont++;
+    let ul = document.getElementById("list")
+    let li = document.createElement("li")
+    li.classList = "lists"
+    li.innerHTML = `
+      <input type="checkbox" />
+      <div class="contentLi" >${input.value}</div>
+      <div class="buttonDelete"><div/>
+    `
+    ul.appendChild(li)
 
-    localStorage.setItem("stringone", JSON.stringify(conteudoSalvo));
-    localStorage.setItem("storage", JSON.stringify(real));
-    localStorage.setItem("numero", cont);
+    let lista = document.querySelectorAll(".lists")
+
+    lista.forEach((element) => {
+
+      element.children[0].addEventListener("click", ()=>{
+        if(element.children[0].checked == true){
+          element.children[1].style.textDecoration = "line-through"
+        }else{
+          element.children[1].style.textDecoration = "none"
+        }
+      })
+
+
+      element.children[2].addEventListener("click", () => {
+
+        $(element).fadeOut(1000)
+        setTimeout(()=>{
+          element.remove()
+        }, 1000)
+        
+      })
+    })
+
+    input.value = ""
+    input.focus()
   }
+
+
 }
 
-if (localStorage.getItem("storage") !== null) {
-  let content = JSON.parse(localStorage.getItem("storage"));
-
-  for (let i = 0; i < content.length; i++) {
-    criarElemento(content[i].content);
+input.addEventListener("keydown", (click)=>{
+  if(click.which == 13){
+    addElement()
   }
-}
+
+  return click
+})
+
+
+buttonAdd.addEventListener("click", addElement)
+
