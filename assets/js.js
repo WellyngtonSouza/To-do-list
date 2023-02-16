e2b103a096ab4c425c9dba0e13af2c46591f3226
wellyngton souza <wellyngtonmonteiro84@gmail.com>
2023-02-16 16:11:53 -0300

    adidicionando local storage, ainda não terminei

--- assets/js.js
+++ assets/js.js
@@ -2,9 +2,52 @@ const buttonAdd = document.getElementById("button")
 const buttonClear = document.getElementById("limpar")
 const input = document.getElementById("input")
 
+let elementSave = []
+let lista; 
+
+if (localStorage.getItem("value") != null) {
+  let get = JSON.parse(localStorage.getItem("value"))
+
+  elementSave = get
+
+  elementSave.forEach((element) => {
+    console.log(element.newObject)
+    let ul = document.getElementById("list")
+    let li = document.createElement("li")
+    li.classList = "lists"
+    li.innerHTML = `
+      <input type="checkbox" />
+      <div class="contentLi" >${element.newObject}</div>
+      <div class="buttonDelete"><div/>
+    `
+    ul.appendChild(li)
+
+    let listaAdd = document.querySelectorAll(".lists")
+
+    listaAdd.forEach((element, i)=>{
+      element.children[0].addEventListener("click", () => {
+        if (element.children[0].checked == true) {
+          element.children[1].style.textDecoration = "line-through"
+        } else {
+          element.children[1].style.textDecoration = "none"
+        }
+      })
+
+      lista = document.querySelectorAll(".lists")
+    })
+
+  })
+
+  
+
+}
 
 
-
+function newObject(newObject) {
+  return {
+    newObject
+  }
+}
 
 let addElement = function () {
   if (input.value == "") {
@@ -21,26 +64,30 @@ let addElement = function () {
     `
     ul.appendChild(li)
 
-    let lista = document.querySelectorAll(".lists")
+    elementSave.push(newObject(li.children[1].textContent))
+
+    localStorage.setItem("value", JSON.stringify(elementSave))
+
+    lista = document.querySelectorAll(".lists")
 
     lista.forEach((element) => {
 
-      element.children[0].addEventListener("click", ()=>{
-        if(element.children[0].checked == true){
+      element.children[0].addEventListener("click", () => {
+        if (element.children[0].checked == true) {
           element.children[1].style.textDecoration = "line-through"
-        }else{
+        } else {
           element.children[1].style.textDecoration = "none"
         }
       })
 
 
       element.children[2].addEventListener("click", () => {
-
+        
         $(element).fadeOut(1000)
-        setTimeout(()=>{
+        setTimeout(() => {
           element.remove()
         }, 1000)
-        
+
       })
     })
 
@@ -51,24 +98,26 @@ let addElement = function () {
 
 }
 
-input.addEventListener("keydown", (click)=>{
-  if(click.which == 13){
+input.addEventListener("keydown", (click) => {
+  if (click.which == 13) {
     addElement()
   }
 
   return click
 })
 
-let clear = function(){
-
-  document.querySelectorAll(".lists").forEach((element)=>{
+let clearAll = function () {
+  localStorage.clear("value")
+  elementSave = []
+  document.querySelectorAll(".lists").forEach((element) => {
     $(element).fadeOut(1000)
-    setTimeout(()=>element.remove(), 1000)
+
+    setTimeout(() => element.remove(), 1000)
   })
 }
 
 
 buttonAdd.addEventListener("click", addElement)
-buttonClear.addEventListener("click", clear)
+buttonClear.addEventListener("click", clearAll)
 
 
